278ba9821e9c153372e784c18babe3d5051796e2
xiongyu <xiongyu@espressif.com>
2020-08-13 17:40:13 +0800

    add delay

--- projects/code/main/main.lua
+++ projects/code/main/main.lua
@@ -25,6 +25,22 @@ if (apds.read('distance') > 200) then
     end
 end
 
+local last_1s = os.time()
+local sys_stats = sys.stats(10)
+function delay(ms)
+    for i = 0, ms / 10, 1 do
+        if (os.difftime (os.time(), last_1s) >= 1) then
+            sys_info = sys.info()
+            lcd.write('STATE', os.date("%H:%M:%S"))
+            print(string.format('RAM left: %dB', tonumber(sys_info.total_heap)))
+            print(string.format('CPU load: %d%%', 100 - sys_stats.IDLE0.load))
+            print(string.format('Distance: %d', apds.read('distance')))
+            last_1s = os.time()
+        end
+        sys_stats = sys.stats(10)
+    end
+end
+
 lcd.print('')
 while (1) do
     for i = 0, 2, 1 do
@@ -38,35 +54,35 @@ while (1) do
         
         for x = 0, 255, 1 do
             led.write(x, 0, 0)
-            sys.delay(10)
+            delay(10)
         end
         for x = 0, 255, 1 do
             led.write(255 - x, 0, 0)
-            sys.delay(10)
+            delay(10)
         end
         for x = 0, 255, 1 do
             led.write(0, x, 0)
-            sys.delay(10)
+            delay(10)
         end
         for x = 0, 255, 1 do
             led.write(0, 255 - x, 0)
-            sys.delay(10)
+            delay(10)
         end
         for x = 0, 255, 1 do
             led.write(0, 0, x)
-            sys.delay(10)
+            delay(10)
         end
         for x = 0, 255, 1 do
             led.write(0, 0, 255 - x)
-            sys.delay(10)
+            delay(10)
         end
         for x = 0, 255, 1 do
             led.write(x, x, x)
-            sys.delay(10)
+            delay(10)
         end
         for x = 0, 255, 1 do
             led.write(255 - x, 255 - x, 255 - x)
-            sys.delay(10)
+            delay(10)
         end
     end
 end
